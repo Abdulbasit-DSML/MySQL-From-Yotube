f6e9112e66f975d92894ccb13939eb873964eb34
abdulbasit shaikh <316396basit@gmail.com>
2026-06-18 23:52:11 +0500

    Update basics.sql

--- basics.sql
+++ basics.sql
@@ -1,3 +1,11 @@
 -- SELECT STATEMENET
 SELECT * 
 FROM parks_and_recreation.employee_demographics;
+
+-- selecting a specific column
+SELECT first_name
+FROM employee_demographics;
+
+-- Adding more columns
+SELECT last_name, first_name, gender, age
+FROM employee_demographics;

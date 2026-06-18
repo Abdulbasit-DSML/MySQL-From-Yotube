5ebcc41c030897d12b7717cf8fef09c4f9789b85
abdulbasit shaikh <316396basit@gmail.com>
2026-06-18 23:55:41 +0500

    Enhance basics.sql with calculations and examples
    Added SQL queries for calculations and PEMDAS rules.

--- basics.sql
+++ basics.sql
@@ -9,3 +9,19 @@ FROM employee_demographics;
 -- Adding more columns
 SELECT last_name, first_name, gender, age
 FROM employee_demographics;
+
+-- Calculations using colmuns
+-- You can see here we have the total_money_spent we can perform calculations on this
+SELECT first_name,
+ last_name,
+ total_money_spent,
+ total_money_spent + 100
+FROM customers;
+
+-- PEMDAS rules
+SELECT first_name, 
+last_name,
+salary,
+(salary + 100) * 10
+FROM employee_salary;
+

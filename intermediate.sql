edaead9da233b6be274d21469d067e64da197a92
abdulbasit shaikh <316396basit@gmail.com>
2026-06-21 00:01:55 +0500

    Implement salary adjustment based on memo guidelines
    Added logic to calculate new salary based on bonus structure.

--- intermediate.sql
+++ intermediate.sql
@@ -7,3 +7,15 @@ CASE
 	WHEN age <= 30 THEN 'Young'
 END
 FROM employee_demographics;
+
+-- Pawnee Council sent out a memo of their bonus and pay increase structure so we need to follow it
+-- Basically if they make less than 45k then they get a 5% raise - very generous
+-- if they make more than 45k they get a 7% raise
+-- they get a bonus of 10% if they work for the Finance Department
+
+SELECT first_name, last_name, salary,
+CASE
+	WHEN salary > 45000 THEN salary + (salary * 0.05)
+    WHEN salary < 45000 THEN salary + (salary * 0.07)
+END AS new_salary
+FROM employee_salary;

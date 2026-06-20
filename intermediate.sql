f10a752b438e8a6b58045d7d48e17327ab0459bd
abdulbasit shaikh <316396basit@gmail.com>
2026-06-21 00:05:33 +0500

    Introduce aliasing in SQL join query
    Added a query using aliasing for better readability.

--- intermediate.sql
+++ intermediate.sql
@@ -42,3 +42,9 @@ JOIN employee_salary
 	ON employee_demographics.employee_id = employee_salary.employee_id;
 
 
+-- use aliasing!
+SELECT *
+FROM employee_demographics dem
+INNER JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
+

3de420f40ecbbb70601e5414af6a96dfa91eca81
abdulbasit shaikh <316396basit@gmail.com>
2026-06-23 23:53:56 +0500

    Add subquery examples for employee demographics
    Added examples of using subqueries in SQL for aggregating employee demographics.

--- intermediate.sql
+++ intermediate.sql
@@ -153,7 +153,21 @@ salary,
 	FROM employee_salary)
 FROM employee_salary;
 
+-- We can also use it in the FROM Statement
+-- when we use it here it's almost like we are creating a small table we are querying off of
+SELECT *
+FROM (SELECT gender, MIN(age), MAX(age), COUNT(age),AVG(age)
+FROM employee_demographics
+GROUP BY gender) 
+;
+-- now this doesn't work because we get an error saying we have to name it
 
+SELECT gender, AVG(Min_age)
+FROM (SELECT gender, MIN(age) Min_age, MAX(age) Max_age, COUNT(age) Count_age ,AVG(age) Avg_age
+FROM employee_demographics
+GROUP BY gender) AS Agg_Table
+GROUP BY gender
+;
 
 
 

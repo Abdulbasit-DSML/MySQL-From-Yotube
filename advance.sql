d520f3e617cb1c44313d25b68ba61a2949012f2c
abdulbasit shaikh <316396basit@gmail.com>
2026-06-29 22:22:58 +0500

    Implement CTE for average salary calculation by gender
    Added a Common Table Expression (CTE) to calculate average salary by gender using aggregated data.

--- advance.sql
+++ advance.sql
@@ -9,3 +9,19 @@ JOIN employee_salary sal
 	ON dem.employee_id = sal.employee_id
 GROUP BY gender
 )
+
+-- Now we can use the columns within this CTE to do calculations on this data that
+-- we couldn't have done without it.
+
+WITH CTE_Example AS 
+(
+SELECT gender, SUM(salary), MIN(salary), MAX(salary), COUNT(salary)
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+-- notice here I have to use back ticks to specify the table names  - without them it doesn't work
+SELECT gender, ROUND(AVG(`SUM(salary)`/`COUNT(salary)`),2)
+FROM CTE_Example
+GROUP BY gender;

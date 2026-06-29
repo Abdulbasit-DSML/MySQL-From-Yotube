7dd5846b937b050f16e4f4c1cb2820705ab5d143
abdulbasit shaikh <316396basit@gmail.com>
2026-06-29 22:21:36 +0500

    Add CTE example for employee salary summary
    Added a Common Table Expression (CTE) example to summarize employee demographics and salaries.

---
+++ advance.sql
@@ -0,0 +1,11 @@
+-- CTE's
+-- First, CTEs start using a "With" Keyword. Now we get to name this CTE anything we want
+-- Then we say as and within the parenthesis we build our subquery/table we want
+WITH CTE_Example AS 
+(
+SELECT gender, SUM(salary), MIN(salary), MAX(salary), COUNT(salary), AVG(salary)
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
